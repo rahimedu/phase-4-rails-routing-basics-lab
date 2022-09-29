eb011d216c33bac09007b23223ff7bbe0505280c
rahimedu <edwin.duche82@gmail.com>
2022-09-28 23:56:04 -0500

    done

---
+++ app/controllers/student controller.rb
@@ -0,0 +1,10 @@
+class StudentController < ApplicationController
+    def index
+        students=Student.all 
+        render json: students
+    end
+    def grades 
+        students=Student.all.order(grade: :desc)
+        render json: students
+    end
+end
